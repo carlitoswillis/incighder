e69aea794ac365d411d20a3e6d1d90a453f71e25
Carlitos Willis <carlitoswillis@CarlitoacStudio.lan>
2025-07-06 12:33:47 -0700

    add more spotify data

--- data-api/spotify_search.py
+++ data-api/spotify_search.py
@@ -24,6 +24,19 @@ def search_artist(sp, query):
         # Return only the necessary fields to keep the payload small
         simplified_results = []
         for item in results['artists']['items']:
+            top_track_name = None
+            top_track_popularity = None
+            top_track_id = None
+
+            # Fetch top tracks for the artist
+            top_tracks = sp.artist_top_tracks(item['id'])
+            if top_tracks and top_tracks['tracks']:
+                # Assuming the first track is the top track
+                top_track = top_tracks['tracks'][0]
+                top_track_name = top_track['name']
+                top_track_popularity = top_track['popularity']
+                top_track_id = top_track['id']
+
             simplified_results.append({
                 'id': item['id'],
                 'name': item['name'],
@@ -31,7 +44,10 @@ def search_artist(sp, query):
                 'genres': item['genres'],
                 'popularity': item['popularity'],
                 'followers': item['followers'],
-                'external_urls': item['external_urls']
+                'external_urls': item['external_urls'],
+                'top_track_name': top_track_name,
+                'top_track_popularity': top_track_popularity,
+                'top_track_id': top_track_id
             })
         return {"artists": {"items": simplified_results}}
     except Exception as e:
